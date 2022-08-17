493d7574a657b8a96a1667f2a15949ff84f83406
Calin Florin TUDOR <calinf.tudor@gmail.com>
2022-08-17 17:39:00 +0300

    2nd solution

--- 8 kyu/reduce_but_grow.js
+++ 8 kyu/reduce_but_grow.js
@@ -2,6 +2,19 @@
 
 // [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 
+// 1st solution:
+
 function grow(x){
   return  x.reduce((acc, current) => acc * current);
+}
+
+// 2nd solution:
+
+function grow(x) {
+  let result = 1;
+
+  for(let i = o; i < x.length; i++) {
+    result *= x[i];
+  }
+  return result;
 }
